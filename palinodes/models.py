3a2b8df31729ed5008f8404bfd2e79242914a761
OscarCarde <oscrdns@gmail.com>
2023-08-11 16:41:41 +0100

    test for getting a directory's path

--- palinodes/models.py
+++ palinodes/models.py
@@ -1,23 +1,49 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 class User(AbstractUser):
     pass
 
-class Member(models.Model):
+class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    avatar = models.ImageField(upload_to="media/avatar", blank=True)
 
-class Song(models.Model):
+
+####### Hierarchical directory structure ########
+
+def get_file_upload_path(instance, filename):
+    return f"{instance.parent_folder.path}/{filename}"
+
+class Repository(models.Model):
     name = models.CharField(max_length=50)
-    lyrics = models.TextField()
-    tempo = models.PositiveIntegerField()
-    file = models.FileField()
+    created = models.DateField(auto_now=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="repositories")
+    collaborators = models.ManyToManyField(User, blank=True, related_name="collaborating")
+
+class Directory(models.Model):
+    name = models.CharField(max_length=50)
+    parent_folder = models.ForeignKey("self", null= True, blank=True, on_delete=models.CASCADE, related_name="subdirectories")
+    parent_repository = models.ForeignKey(Repository, on_delete=models.CASCADE, related_name="directories")
 
     @property
-    def length(self):
-        return 1.0 
+    def path(self):
+        if not self.parent_folder:
+            return f"{self.parent_repository.name}/{self.name}"
+        else:
+            return f"{self.parent_folder.path}/{self.name}"
+
+class File(models.Model):
+    parent_folder = models.ForeignKey(Directory, null= True, blank=True, on_delete=models.CASCADE, related_name="files")
+    parent_repository = models.ForeignKey(Repository, on_delete=models.CASCADE, related_name="contents")
+    file = models.FileField(upload_to=get_file_upload_path)
+
+    def path(self):
+        return self.file.url
     
+###############################################
+
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name="comments")
-    song = models.ForeignKey(Song, on_delete=models.CASCADE, related_name="comments")
+    file = models.ForeignKey(File, on_delete=models.CASCADE, related_name="comments")
     comment = models.TextField()
--- palinodes/tests.py
+++ palinodes/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .models import *
 
-# Create your tests here.
+class DirectoryTestCase(TestCase):
+
+    def setUp(self):
+        self.user1 = User(username = "Alice")
+        self.repo = Repository(name="repo", owner = self.user1)
+        self.dir1 = Directory(name = "dir1", parent_repository = self.repo)
+        self.dir2 = Directory(name = "dir11", parent_repository = self.repo, parent_folder=self.dir1)
+        self.dir3 = Directory(name = "dir111", parent_repository = self.repo, parent_folder=self.dir2)
+
+    def test_directory_path(self):
+
+        path1 = self.dir1.path
+        self.assertEquals(f"{self.repo.name}/dir1", path1)
+        path2 = self.dir2.path
+        self.assertEquals(f"{self.repo.name}/dir1/dir11", path2)
+        path3 = self.dir3.path
+        self.assertEquals(f"{self.repo.name}/dir1/dir11/dir111", path3)
