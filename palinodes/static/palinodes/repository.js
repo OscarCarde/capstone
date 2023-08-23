1c17676f084867a42c19e8a84ad475d49eb8f4c2
OscarCarde <oscrdns@gmail.com>
2023-08-23 19:33:06 +0100

    navigating through subdirectories js

--- palinodes/static/palinodes/repository.js
+++ palinodes/static/palinodes/repository.js
@@ -1,33 +1,39 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelectorAll('.directories').forEach(directory => {
+        console.log(directory.dataset.pk)
         directory.addEventListener('click', () => {
-            loadDirectoryContents(this.dataset.pk)
+            loadDirectoryContents(directory.dataset.pk)
         });
     })
 })
 
-function loadDirectoryContents(directory_pk) {
+async function loadDirectoryContents(directory_pk) {
     document.querySelector("#contents").replaceChildren()
     //get the contents from the directory with primary key directory_pk
     //create a list of elements with the directory's contents
-    fetch(`/directory/${directory_pk}`)
-    .then(response => response.json)
+    await fetch(`/directory/${directory_pk}`)
+    .then(response => response.json())
     .then(data => {
         data.subdirectories.forEach(subdirectory => {
             let container = document.createElement('div');
             //TODO: find out how to add the folder icon 
-            let folderIcon = document.getElementById("folder-icon").cloneNode(true);
+            let hiddenfolderIcon = document.querySelector("#folder-icon");
+            let folderIcon = hiddenfolderIcon.cloneNode(true);
             folderIcon.style.display = "block";
             let directory = document.createElement('p');
             directory.innerHTML = subdirectory.name;
             container.append(folderIcon, directory);
             container.setAttribute("data-pk", subdirectory.pk);
             container.className = "directories file-entries";
+            container.addEventListener('click', () => {
+                loadDirectoryContents(container.dataset.pk)
+            });
             document.getElementById("contents").append(container);
         })
         data.files.forEach(file => {
             let container = document.createElement('div');
-            let fileIcon = document.getElementById("file-icon").cloneNode(true);
+            let hiddenfileIcon = document.querySelector("#file-icon");
+            let fileIcon = hiddenfileIcon.cloneNode(true);
             fileIcon.style.display = "block";
             let filename = document.createElement('p');
             filename.innerHTML = file.filename;
